b1fdb0abceb7f8d66fa200d7965342f842630945
Mohamed Testoury <mohamed.testouri@esprit.tn>
2020-11-17 21:48:23 +0100

    login and sign up are done

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,16 +1,18 @@
 /***** GLOBAL IMPORTS *****/
+const { hash } = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 /***** MODELS IMPORT *****/
 const User = require('../models/user');
 
 /***** GET REQUEST *****/
 /*** GET A USER ***/
-router.get('/:idUser', (req, res, next) => {
+router.get('/find/:idUser', (req, res, next) => {
     const id = req.params.idUser;
-    User.findById(id).exec.then(doc => {
+    User.findById(id).exec().then(doc => {
         if (doc) {
             console.log(doc);
             res.status(200).json(doc);
@@ -23,41 +25,90 @@ router.get('/:idUser', (req, res, next) => {
     });
 });
 
-/***** POST RESQUEST *****/
-/*** POST A USER ***/
-router.post('/', (req, res, next) => {
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        fullName: req.body.fullName,
-        phone: req.body.phone,
-        email: req.body.email,
-        password: req.body.password,
-        sexe: req.body.sexe,
-        birthday: req.body.birthday,
-        healthInformation: [{
-            calories: req.body.healthInformation.calories,
-            date: Date.now(),
-            weight: req.body.healthInformation.weight,
-            height: req.body.healthInformation.height,
-        }],
-        runs: [{
-            calories: req.body.runs.calories,
-            distance: req.body.runs.distance,
-            duration: req.body.runs.duration,
-            date: Date.now()
-        }],
-        activities: [{
-            sum: req.body.sum,
-            _idExercice: req.body.activities._idExercice,
-        }],
+/***** LOGIN AND SIGNUP *****/
+/*** SIGNUP ***/
+router.post('/signup', (req, res, next) => {
+    User.find({ email: req.body.email }).exec().then(user => {
+        if (user >= 1) {
+            return res.status(409).json({
+                message: 'Mail Exists',
+            });
+        } else {
+            bcrypt.hash(req.body.password, 10, (error, hash) => {
+                if (error) {
+                    return res.status(500).json({
+                        error: error,
+                    });
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        fullName: req.body.fullName,
+                        phone: req.body.phone,
+                        email: req.body.email,
+                        password: hash,
+                        sexe: req.body.sexe,
+                        birthday: req.body.birthday,
+                        healthInformation: [{
+                            calories: req.body.healthInformation.calories,
+                            date: Date.now(),
+                            weight: req.body.healthInformation.weight,
+                            height: req.body.healthInformation.height,
+                        }],
+                        runs: [{
+                            calories: req.body.runs.calories,
+                            distance: req.body.runs.distance,
+                            duration: req.body.runs.duration,
+                            date: Date.now()
+                        }],
+                        activities: [{
+                            sum: req.body.sum,
+                            _idExercice: req.body.activities._idExercice,
+                        }],
+                    });
+                };
+            });
+        };
     });
     user.save().then(result => {
         console.log(result);
         res.status(201).json({
+            message: "User Created",
             user: result,
         });
     }).catch(error => console.log(error));
 });
+
+/*** LOGIN ***/
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email }).exec().then(user => {
+        if (user.length < 1) { return res.status(401).json({ message: 'Mail not found', }); }
+        bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+            if (error) {
+                return res.status(401).json({
+                    message: 'Mail not found',
+                });
+            }
+            if (result) {
+                const token = jwt.sign({
+                    email: user[0].email,
+                    idUser: user[0]._id,
+                }, process.env.JWT_KEY, {
+                    expiresIn: "24h"
+                });
+                return res.status(200).json({
+                    message: 'Successful',
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message: 'Mail not found',
+            });
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({ error: error });
+    });
+})
 
 /***** PUT RESQUEST *****/
 /*** PUT A RUN ***/
@@ -131,10 +182,5 @@ router.delete('/:idUser', (req, res, next) => {
         res.status(500).json({ error: error });
     });
 });
-
-/***** LOGIN AND SIGNUP *****/
-/*** SIGNUP ***/
-
-/*** LOGIN ***/
 
 module.exports = router;
