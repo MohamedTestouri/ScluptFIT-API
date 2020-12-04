864db0efc55d76bdef7d1cdc358379799c2961db
Mohamed Testoury <mohamed.testouri@esprit.tn>
2020-12-04 23:10:47 +0100

    test fixing Post Request

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 /***** UTILS CONFIG *****/
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /***** DATABASE CONNECTION *****/
