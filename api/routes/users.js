0fade0222a4f0b8317a09493fa7d223e547cb899
Mohamed Testoury <mohamed.testouri@esprit.tn>
2021-01-11 21:18:20 +0100

    user send

--- api/routes/users.js
+++ api/routes/users.js
@@ -95,8 +95,8 @@ router.post('/login', (req, res, next) => {
                 return res.status(200).json({
                     //message: 'Successful',
                     //token: token,
-                   // user: user[0],
-                    id : user[0]._id
+                    user: user[0],
+                   // id : user[0]._id
                 });
             }
             res.status(401).json({
